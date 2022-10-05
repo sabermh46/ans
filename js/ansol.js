4140b2e8b1e24b232cbe0345257ae5c2a593e3fd
Mohammad Saber Mahmud Sourav <74147777+sabermh46@users.noreply.github.com>
2022-10-06 00:50:17 +0600

    Update ansol.js

--- js/ansol.js
+++ js/ansol.js
@@ -10,31 +10,66 @@ var nav_bar = document.querySelector('.navigation_bar');
 /* SHORT CARD CONTAINER START */
 
 function short_card_slide(){
-    
+
+    var short_card_container = document.querySelector('.short_card_container');
+    var short_card_frame = document.querySelector('.short_card_container .frame');
+
     var short_card_slider = document.querySelector('.short_card_container .frame .slider');
     var short_card_slider_left_btn = document.querySelector('.short_card_container .left_btn');
     var short_card_slider_right_btn = document.querySelector('.short_card_container .right_btn');
 
 
-    var short_frame_width = document.querySelector('.short_card_container .frame').getBoundingClientRect().width;
+    
+    var slider_width = short_card_slider.getBoundingClientRect().width;
 
-    var slider_info = short_card_slider.getBoundingClientRect();
-    var slider_width = slider_info.width;
-    var slider_content_count = short_card_slider.querySelectorAll('.short_card').length;
+    var slider_content = short_card_slider.querySelectorAll('.short_card');
+    var content_width;
+    slider_content.forEach(cont =>{
+        content_width = cont.getBoundingClientRect().width;
+    })
+    
 
-    var sliderUnitMove = (slider_width + slider_content_count) / slider_content_count;
+    
     var slider_current_pos = 0;
-    var slider_rem_pos;
 
+
+    var screen_width = window.screen.width;
+    console.log('screen_width: ' + window.screen.width)
+
+    var effective_width, gap;
+    if(screen_width > 1200){
+        effective_width = 1200
+        gap = 20
+    } else if(screen_width < 1200 && screen_width > 760){
+        effective_width = screen_width - 200
+        gap = 20
+    } else if(screen_width < 760){
+        effective_width = 350
+        gap = 0
+    }
+
+    var sliderUnitMove = content_width+gap;
+
+    var showing_element = parseInt(effective_width / (content_width+gap))
+    console.log(showing_element)
+
+    short_card_slider.style.gap = `${gap}px`
+
+    short_card_frame.style.width = `${ showing_element*content_width + (showing_element-1)*gap }px`;
+    short_card_container.style.width = `${ showing_element*content_width + (showing_element-1)*gap + 100 }px`;
+
+    var short_frame_width = short_card_frame.getBoundingClientRect().width;
 
     short_card_slider_right_btn.addEventListener('click', ()=>{
+
+        console.log('Top of left working',
+            'left Cond: '+(slider_current_pos + short_frame_width + sliderUnitMove),
+            'right cond: '+ slider_width
+        );
         
         if(slider_current_pos + short_frame_width + sliderUnitMove < slider_width) {
             slider_current_pos += sliderUnitMove;
-        }
-        else {
-            slider_rem_pos = (slider_width - (slider_current_pos + short_frame_width))
-            slider_current_pos += slider_rem_pos
+            console.log('Conditional working');
         }
 
         short_card_slider.style.transform = `translateX(-${slider_current_pos}px)`;
@@ -56,11 +91,7 @@ function short_card_slide(){
 
 
     console.log(
-        slider_width,
-        short_card_slider,
-        short_card_slider_left_btn,
-        short_card_slider_right_btn,
-        slider_content_count
+        'slider_width: '+slider_width,
     )
 }
 
@@ -75,30 +106,65 @@ short_card_slide();
 
 function course_card_slide(){
     
+    var course_card_container = document.querySelector('.course_card_container');
+    var course_card_frame = document.querySelector('.course_card_container .frame');
+
     var course_card_slider = document.querySelector('.course_card_container .frame .slider');
     var course_card_slider_left_btn = document.querySelector('.course_card_container .left_btn');
     var course_card_slider_right_btn = document.querySelector('.course_card_container .right_btn');
 
 
-    var course_frame_width = document.querySelector('.course_card_container .frame').getBoundingClientRect().width;
+    
+    var slider_width = course_card_slider.getBoundingClientRect().width;
 
-    var slider_info = course_card_slider.getBoundingClientRect();
-    var slider_width = slider_info.width;
-    var slider_content_count = course_card_slider.querySelectorAll('.course-card').length;
+    var slider_content = course_card_slider.querySelectorAll('.course-card');
+    var content_width;
+    slider_content.forEach(cont =>{
+        content_width = cont.getBoundingClientRect().width;
+    })
+    
 
-    var sliderUnitMove = (slider_width + slider_content_count) / slider_content_count;
+    
     var slider_current_pos = 0;
-    var slider_rem_pos;
 
+
+    var screen_width = window.screen.width;
+    console.log('screen_width: ' + window.screen.width)
+
+    var effective_width, gap;
+    if(screen_width > 1200){
+        effective_width = 1200
+        gap = 40
+    } else if(screen_width < 1200 && screen_width > 760){
+        effective_width = screen_width - 200
+        gap = 30
+    } else if(screen_width < 760){
+        effective_width = 390
+        gap = 0
+    }
+
+    var sliderUnitMove = content_width+gap;
+
+    var showing_element = parseInt(effective_width / (content_width+gap))
+    console.log(showing_element)
+
+    course_card_slider.style.gap = `${gap}px`
+
+    course_card_frame.style.width = `${ showing_element*content_width + (showing_element-1)*gap }px`;
+    course_card_container.style.width = `${ showing_element*content_width + (showing_element-1)*gap + 100 }px`;
+
+    var course_frame_width = course_card_frame.getBoundingClientRect().width;
 
     course_card_slider_right_btn.addEventListener('click', ()=>{
+
+        console.log('Top of left working',
+            'left Cond: '+(slider_current_pos + course_frame_width + sliderUnitMove),
+            'right cond: '+ slider_width
+        );
         
         if(slider_current_pos + course_frame_width + sliderUnitMove < slider_width) {
             slider_current_pos += sliderUnitMove;
-        }
-        else {
-            slider_rem_pos = (slider_width - (slider_current_pos + course_frame_width))
-            slider_current_pos += slider_rem_pos
+            console.log('Conditional working');
         }
 
         course_card_slider.style.transform = `translateX(-${slider_current_pos}px)`;
@@ -119,13 +185,6 @@ function course_card_slide(){
 
 
 
-    console.log(
-        slider_width,
-        course_card_slider,
-        course_card_slider_left_btn,
-        course_card_slider_right_btn,
-        slider_content_count
-    )
 }
 
 course_card_slide();
