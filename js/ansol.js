af0bc8e3578b94916c78adf513885367b1c91fd6
Mohammad Saber Mahmud Sourav <74147777+sabermh46@users.noreply.github.com>
2022-10-16 03:26:12 +0600

    Update

--- js/ansol.js
+++ js/ansol.js
@@ -148,14 +148,14 @@ window.onscroll = (e)=> {
 
     if (y < 250){
         nav_bar.style.height = `${ 200-y*0.5 }px`;
-        nav_bar.style.padding = `0px ${ sW > 450 ? (120-y*0.3) : (20-y*0.04) }px`;
+        nav_bar.style.padding = `0px ${ sW > 700 ? (120-y*0.3) : (20-y*0.04) }px`;
         nav_bar.style.background = 'transparent';
         nav_bar.style.boxShadow = '0px 1px 8px rgba(0, 0, 0, 0.0)';
         logo.style.width = sW > 450 ? `${ 200-y*0.5 }px` : `${ 120-y*0.1 }px`
     } 
     if (y > 250){
         nav_bar.style.height = '75px';
-        nav_bar.style.padding = `0px ${ sW > 450 ? 20 : 3 }px`;
+        nav_bar.style.padding = `0px ${ sW > 700 ? 20 : 3 }px`;
         nav_bar.style.background = '#E9F4FF';
         nav_bar.style.boxShadow = '0px 1px 8px rgba(0, 0, 0, 0.25)';
         logo.style.width = `100px`
